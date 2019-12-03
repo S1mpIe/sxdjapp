545b922106be279a30e30a2498113a6033c35060
S1mpIe <54990505+S1mpIe@users.noreply.github.com>
2019-12-03 20:42:39 +0800

    Init...

---
+++ src/main/java/com/finals/sxdj/SxdjApplication.java
@@ -0,0 +1,13 @@
+package com.finals.sxdj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxdjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SxdjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finals/sxdj/SxdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finals.sxdj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SxdjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
