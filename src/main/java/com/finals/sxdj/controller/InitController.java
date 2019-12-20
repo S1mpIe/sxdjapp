c144801505a19102b4748b0a25223d20abeacf5b
S1mpIe <54990505+S1mpIe@users.noreply.github.com>
2019-12-20 11:14:51 +0800

    登录1.1

--- src/main/java/com/finals/sxdj/controller/InitController.java
+++ src/main/java/com/finals/sxdj/controller/InitController.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -19,12 +20,12 @@ public class InitController {
     private InitService initService;
 
     @ResponseBody
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login",method = RequestMethod.PUT)
     public String getLogin(@Param("code") String code){
         return initService.login(code).toJSONString();
     }
     @ResponseBody
-    @RequestMapping("/register")
+    @RequestMapping(value = "/register",method = RequestMethod.PUT)
     public String registerUser(User user){
         return initService.register(user).toJSONString();
     }
