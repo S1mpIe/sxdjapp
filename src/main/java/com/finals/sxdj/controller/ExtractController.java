213e46daaa49daf848d70fb647e2f6c8c20a0aa9
S1mpIe <54990505+S1mpIe@users.noreply.github.com>
2020-02-04 15:37:27 +0800

    Init...

--- src/main/java/com/finals/sxdj/controller/ExtractController.java
+++ src/main/java/com/finals/sxdj/controller/ExtractController.java
@@ -1,11 +1,24 @@
 package com.finals.sxdj.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.finals.sxdj.model.sqlmodel.ExtractPoint;
+import com.finals.sxdj.services.ExtractService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author S1mpIe
  */
 @Controller
 public class ExtractController {
-
+    @Autowired
+    private ExtractService extractService;
+    @PutMapping("/extract")
+    @ResponseBody
+    public String applyPoint(ExtractPoint point){
+        JSONObject jsonObject = extractService.insertNewAuditedPoint(point);
+        return jsonObject.toJSONString();
+    }
 }
